d1a90fc54f536378acfff974ff76fb391fee7aad
emodarua <emodaesquina@gmail.com>
2026-03-27 22:06:12 -0300

    exercicio 1

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+let bonus_antigos = 300
+let ganhos = 400
+
+pontos_totais = bonus_antigos + ganhos
+console.log(`
+    Os pontos antigos eram de: ${bonus_antigos}
+    Pontos ganhos: ${ganhos}
+    --------------------------------------------
+    Agora os pontos são de: ${pontos_totais}
+    --------------------------------------------
+    `)
