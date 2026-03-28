4e2c3785e86de20ac5dc06f5c5a38d5e31e7a378
emodarua <emodaesquina@gmail.com>
2026-03-27 22:13:57 -0300

    exercicio 2

---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+const nome = prompt('Digite seu nome: ')
+
+function saudar(nome) {
+    console.log(`
+        -----------------------------------------
+        Olá, ${nome}, seja bem vindo!!
+        -----------------------------------------
+        
+        `)
+}
+
+console.log(saudar)
